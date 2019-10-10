666e0d0e8594533d082db6895086e0905babb179
sacrofer <38994980+sacrofer@users.noreply.github.com>
2019-10-10 00:19:16 -0500

    First commint which only includes file creation and Add node

---
+++ ManipulateXML.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace XML_Manipulation
+{
+    class ManipulateXML
+    {
+        XmlDocument doc;
+
+        public string FileName
+        { get; set; }
+
+        public ManipulateXML(string fileName)
+        {
+            FileName = fileName;
+            doc = new XmlDocument();
+
+            CreateFile();
+        }
+
+        private void CreateFile()
+        {
+            if (!File.Exists(FileName))
+            {
+                using FileStream fs = File.Create(FileName);            
+                    Byte[] info = new UTF8Encoding(true).GetBytes("<?xml version=\"1.0\" ?>\n<root>\n</root>");
+                    fs.Write(info, 0, info.Length);                
+            }
+
+            doc.Load(FileName);
+        }
+        public void Add(string element, string innerText, string attribute, string value)
+        {
+            XmlNode root = doc.DocumentElement;
+            XmlElement elem = doc.CreateElement(element);
+            XmlAttribute attr = doc.CreateAttribute(attribute);
+            attr.Value = value;
+            elem.Attributes.Append(attr);
+            elem.InnerText = innerText;
+            root.InsertAfter(elem, root.LastChild);
+            doc.Save(FileName);
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace XML_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string path = Directory.GetCurrentDirectory() + @"\Test.xml";
+            ManipulateXML myXML = new ManipulateXML(path);
+
+            myXML.Add("PROJECT", "My Project", "Version", "1.0");
+        }
+    }
+}
